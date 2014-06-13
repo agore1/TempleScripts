5e6d790059b43e68def438c04a7b5ce16a5e94cc
Austin Gore <austin.gore@gmail.com>
2014-06-13 10:56:20 -0400

    Added email search, commented out the consolidated results printing

--- multisearch.sh
+++ multisearch.sh
@@ -6,20 +6,20 @@ echo "What file would you like to search?"
 read FILENAME
 #imei for galaxy nexus is 351746051667742
 #serial number for galaxy nexus is 0149C22118018016
-KEYWORDS=( mac imei user-agent 351746051667742 0149C22118018016 )
+KEYWORDS=( mac imei user-agent "templetest79@gmail.com" 351746051667742 0149C22118018016 )
 declare -a RESULTS
 for i in "${KEYWORDS[@]}"
 do
 	#search the pcap file for the keyword, and list it's occurrences
 	count=`tcpdump -Ann -r /var/tmp/"$FILENAME".pcap '(src 192.168.1.100 and (tcp or udp))' | grep -iEc "$i"`  
 	echo $i "appeared: " $count " times."
-	RESULTS[$i]=$count
+	# RESULTS[$i]=$count
 done
 
 #print out the contents of the array
-for j in "${RESULTS[@]}"
-do
-	echo ${KEYWORDS[$j]} "appeared: " ${RESULTS[$j]} " times."
-done
+# for j in "${RESULTS[@]}"
+# do
+# 	echo {$KEYWORDS[$j]} "appeared: " ${RESULTS[$j]} " times."
+# done
 
 exit 0
